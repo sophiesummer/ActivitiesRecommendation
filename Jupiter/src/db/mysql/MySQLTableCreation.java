775a3bce9fd002e64f437fb9495a49596401e3ee
yangm2 <yangm2@illinois.edu>
2018-09-09 00:33:41 -0500

    Initial Commit

---
+++ Jupiter/src/db/mysql/MySQLTableCreation.java
@@ -0,0 +1,97 @@
+package db.mysql;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Connection;
+
+public class MySQLTableCreation {
+	// Run as Java application 
+	//reset db schema
+	public static void main(String[] args) {
+		try {
+			// This is java.sql.Connection.
+			Connection conn = null;
+
+			// Connect to MySQL.
+			try {
+				System.out.println("Connecting to " + MySQLDBUtil.URL);
+				Class.forName("com.mysql.jdbc.Driver").getConstructor().newInstance();
+				//running time, reflection
+				conn = DriverManager.getConnection(MySQLDBUtil.URL);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+			if (conn == null) {
+				return;
+			}
+
+			// Drop tables in case they exist.
+			Statement stmt = conn.createStatement();
+			String sql = "DROP TABLE IF EXISTS categories";
+			stmt.executeUpdate(sql);
+			
+			sql = "DROP TABLE IF EXISTS history";
+			stmt.executeUpdate(sql);
+			
+			sql = "DROP TABLE IF EXISTS items";
+			stmt.executeUpdate(sql);
+			
+			sql = "DROP TABLE IF EXISTS users";
+			stmt.executeUpdate(sql);
+			
+			// Create new tables
+			sql = "CREATE TABLE items ("
+					+ "item_id VARCHAR(255) NOT NULL,"
+					+ "name VARCHAR(255),"
+					+ "rating FLOAT,"
+					+ "address VARCHAR(255),"
+					+ "image_url VARCHAR(255),"
+					+ "url VARCHAR(255),"
+					+ "distance FLOAT,"
+					+ "PRIMARY KEY (item_id))";
+			
+			stmt.executeUpdate(sql);
+			
+			sql = "CREATE TABLE categories ("
+					+ "item_id VARCHAR(255) NOT NULL,"
+					+ "category VARCHAR(255) NOT NULL,"
+					+ "PRIMARY KEY (item_id, category),"
+					+ "FOREIGN KEY (item_id) REFERENCES items(item_id))";
+			
+			stmt.executeUpdate(sql);
+
+			sql = "CREATE TABLE users ("
+					+ "user_id VARCHAR(255) NOT NULL,"
+					+ "password VARCHAR(255) NOT NULL,"
+					+ "first_name VARCHAR(255),"
+					+ "last_name VARCHAR(255),"
+					+ "PRIMARY KEY (user_id))";
+			
+			stmt.executeUpdate(sql);
+			
+			sql = "CREATE TABLE history ("
+					+ "user_id VARCHAR(255) NOT NULL,"
+					+ "item_id VARCHAR(255) NOT NULL,"
+					+ "last_favor_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+					+ "PRIMARY KEY (user_id, item_id),"
+					+ "FOREIGN KEY (item_id) REFERENCES items(item_id),"
+					+ "FOREIGN KEY (user_id) REFERENCES users(user_id))";
+			
+			stmt.executeUpdate(sql);
+
+			// Create a fake user
+			sql = "INSERT INTO users VALUES ("
+					+ "'1111', '12345678899009', 'Sophie', 'Meng') ";
+			stmt.executeUpdate(sql);
+			System.out.println("Excuting query :" + sql);
+					
+			
+//			System.out.println("Import is done successfully.");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
